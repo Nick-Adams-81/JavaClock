f760419eedc40435f8c0368772ebc3387fd28b53
Nick Adams <nick.pappas.adams@gmail.com>
2023-08-30 15:53:03 -0700

    frame set up

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,5 +1,14 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class MyFrame extends JFrame {
 
+    MyFrame() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("My Clock");
+        this.setLayout(new FlowLayout());
+        this.setSize(650, 300);
+        this.setResizable(false);
+        this.setVisible(true);
+    }
 }
